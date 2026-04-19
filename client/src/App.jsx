5e3fd6a32b9534523abdf540208c8c5964a39c64
akshay010-svg <akshaynaroliya715@gmail.com>
2026-04-19 21:15:36 +0530

    Completed the Login page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,11 +8,15 @@ import Connections from './pages/Connections'
 import Discover from './pages/Discover'
 import Profile from './pages/Profile'
 import CreatePost from './pages/CreatePost'
+import {useUser} from '@clerk/react'
+import Layout from './pages/Layout'
+
 const App = () => {
+  const {user} = useUser()
   return (
     <>
       <Routes>
-        <Route path = '/' element={<Login />}>
+        <Route path = '/' element={ !user ? <Login /> : <Layout />}>
           <Route index element={<Feed />}/>
           <Route path='messages' element={<Messages />}/>
           <Route path='messages/:userId' element={<ChatBox />}/>
@@ -21,11 +25,10 @@ const App = () => {
           <Route path='profile' element={<Profile />}/>
           <Route path='profile/:profileId' element={<Profile />}/>
           <Route path='create-post' element={<CreatePost />}/>
-          
         </Route>
       </Routes>
     </>
   )
 }
 
-export default App
+export default App;
